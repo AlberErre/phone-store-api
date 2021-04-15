961a62dd57eb8ff204bd9b3a697976fbe2388fb6
Alberto Ruiz <alberto@acidtango.com>
2021-04-15 16:15:10 +0100

    fix: handle cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import phoneRouter from "./api/phone";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.json({ message: "hello!" });
