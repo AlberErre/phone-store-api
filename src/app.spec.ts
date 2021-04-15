5a13dfe87d1a86809f781f359e56af13c3787e27
Alberto Ruiz <alberto@acidtango.com>
2021-04-15 12:24:16 +0100

    test: add Phone type test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,3 +1,4 @@
+import { Phone } from "./phone/domain/Phone";
 import app from "./server";
 import supertest from "supertest";
 
@@ -17,5 +18,20 @@ it("phones should return an array", async (done) => {
 
   expect(response.status).toBe(200);
   expect(Array.isArray(phones)).toBeTruthy();
+  done();
+});
+
+it("phones should return Phone type", async (done) => {
+  const response = await request.get("/phones");
+  const phone: Phone = response.body[0];
+
+  expect(phone.brand).toBeTruthy();
+  expect(phone.name).toBeTruthy();
+  expect(phone.description).toBeTruthy();
+  expect(phone.image).toBeTruthy();
+  expect(phone.price).toBeTruthy();
+  expect(phone.color).toBeTruthy();
+  expect(phone.colorDescription).toBeTruthy();
+
   done();
 });
